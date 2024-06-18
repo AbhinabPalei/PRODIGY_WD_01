b0840a25b7ab6bd1afb7da94edb77d6dd8c725f6
AbhinabPalei <117651128+AbhinabPalei@users.noreply.github.com>
2024-06-18 18:34:50 +0530

    Add files via upload

---
+++ Task1/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+mobileMenu.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
